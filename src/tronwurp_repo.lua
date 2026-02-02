6eed109a5561a12438961595220ac98a6475a194
sunwurp1337 <sunwurp@proton.me>
2026-02-02 13:40:27 +0300

    Update tronwurp_repo.lua

--- src/tronwurp_repo.lua
+++ src/tronwurp_repo.lua
@@ -1,15 +1,5 @@
--- Run once to create local repo config
-local cfg = [[
 return {
-  User   = "sunwurp1337",
-  Repo   = "testUIXRoblox",
+  User = "sunwurp1337",
+  Repo = "testUIXRoblox",
   Branch = "main"
 }
-]]
-
-if writefile then
-  writefile("tronwurp_repo.lua", cfg)
-  print("[TRONWURP] tronwurp_repo.lua written.")
-else
-  warn("[TRONWURP] writefile not available in this environment.")
-end
