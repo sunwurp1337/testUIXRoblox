002492846d6d56498214bd599221824f30bd42a6
sunwurp1337 <sunwurp@proton.me>
2026-02-02 14:27:28 +0300

    Update Manifest.lua

--- src/Core/Manifest.lua
+++ src/Core/Manifest.lua
@@ -2,7 +2,7 @@
 return {
     Metadata = {
         Name = "TRONWURP VIP",
-        Version = "3.0.0",
+        Version = "1.0.0",
     },
     Modules = {
         Core = {"Library"}, -- Manifest zaten yüklü, Library'yi ekledik
